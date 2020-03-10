bdf32b157f44828c0e699f4f1a5f537497df9493
Matheus Muriel <matheus.muriel@outlook.com>
2020-03-10 13:09:06 +0000

    Form to batch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    messageLinks: {}
+  },
+  methods: {
+    generateBatch: function(e) {
+        this.message = "Yeah!";
+    }
   }
 })
