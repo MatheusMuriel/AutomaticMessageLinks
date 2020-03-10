5635d3859d2db37fee9de831f128ce1adbcf77dd
Matheus Muriel <matheus.muriel@outlook.com>
2020-03-10 15:32:58 +0000

    Clicks controls

--- js/script.js
+++ js/script.js
@@ -2,11 +2,38 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
-    messageLinks: {}
+    messageLinks: [
+        {number: 123, link: "#1", clicked: false},
+        {number: 124, link: "#2", clicked: true},
+        {number: 125, link: "#3", clicked: false},
+        {number: 126, link: "#4", clicked: false}
+    ]
   },
   methods: {
     generateBatch: function(e) {
         this.message = "Yeah!";
+    },
+    createLink: function(_number, _message) {
+        // https://api.whatsapp.com/send?phone=SeuNúmero&text=SuaMensagem
+        let prefix = "https://api.whatsapp.com/send";
+        
+        let urlPhone = `phone=${formatNumber(_number)}`;
+        let urlMessage = `text=${formatMessage(_message)}`;
+
+        let _link = `${prefix}?${urlPhone}&${urlMessage}`;
+
+        this.messageLinks.push({ number: _number, link: _link });
+    },
+    setClicked: function(_link) {
+        let index = this.messageLinks.indexOf(_link);
+        this.messageLinks[index].clicked = true;
+    },
+    formatNumber: function(numberOriginal) {
+        // CodCountry+DDD+Number
+    },
+    formatMessage: function(messageOriginal) {
+        // Space = %20
+
     }
   }
 })
