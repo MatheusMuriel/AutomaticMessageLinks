b188a64615e60ba9408b610c1f5ba83bad426ad1
Matheus Muriel <matheus.muriel@outlook.com>
2025-05-26 15:34:28 -0300

    Tradução e melhorias

--- js/script.js
+++ js/script.js
@@ -13,15 +13,18 @@ var app = new Vue({
 
     },
     createLink: function(_number, _message) {
-        // https://api.whatsapp.com/send?phone=SeuNúmero&text=SuaMensagem
-        let prefix = "https://api.whatsapp.com/send";
-        
-        let urlPhone = `phone=${this.formatNumber(_number)}`;
-        let urlMessage = `text=${this.formatMessage(_message)}`;
+      // Referencia de link
+      // https://api.whatsapp.com/send?phone=SeuNúmero&text=SuaMensagem
+      let prefix = "https://api.whatsapp.com/send";
+      
+      let urlPhone = `phone=${this.formatNumber(_number)}`;
+      let urlMessage = `text=${this.formatMessage(_message)}`;
 
-        let _link = `${prefix}?${urlPhone}&${urlMessage}`;
+      let _link = `${prefix}?${urlPhone}&${urlMessage}`;
 
+      if (!this.messageLinks.some(item => item.number === _number)) {
         this.messageLinks.push({ number: _number, link: _link, clicked: false});
+      }
     },
     linkClick: function(_link) {
       let index = this.messageLinks.indexOf(_link);
@@ -30,11 +33,16 @@ var app = new Vue({
       window.open(_link.link) 
     },
     formatNumber: function(numberOriginal) {
-        // CodCountry+DDD+Number
-        return numberOriginal;
+      let charactersToRemove = ['-', '(', ')'];
+
+      let cleaned_string = numberOriginal;
+
+      charactersToRemove.forEach(c => cleaned_string = cleaned_string.replace(c, ''));
+      // Estruturas possiveis: "+"CodCountry+DDD+Number / DDD+Number
+      return cleaned_string;
     },
     formatMessage: function(messageOriginal) {
-        // Space = %20
+      // Space = %20
       let newMessage = messageOriginal.split(/\s/).join("\%20");
 
       return newMessage;
